e691eb56a2db1a4ed1741e4fa4e8a5371fc9df3c
Jakub Černý <AsKronak@seznam.cz>
2025-04-11 10:43:22 +0200

    init

---
+++ WheaterApp.Client/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createPinia } from "pinia";
+import router from "./router/router.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const app = createApp(App);
+const pinia = createPinia();
+app.use(pinia);
+app.use(router);
+app.mount("#app");
