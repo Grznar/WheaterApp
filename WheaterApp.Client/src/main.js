3acce3731a4f3551a5c98173fc99849a4ac75306
Jakub Černý <AsKronak@seznam.cz>
2025-04-11 10:51:02 +0200

    sass bootstrap

--- WheaterApp.Client/src/main.js
+++ WheaterApp.Client/src/main.js
@@ -2,10 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router/router.js";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "bootstrap-icons/font/bootstrap-icons.css";
 
+import "@/assets/_custom.scss";
 const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia);
